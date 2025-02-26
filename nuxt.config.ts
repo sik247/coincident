3659b6f645a8280ba30244ceadd6fa517585cfdb
Lee Ki Won <cute01235@nate.com>
2025-02-26 16:43:25 +0900

    [update] seo 및 메타태그 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,33 @@
 export default defineNuxtConfig({
-  css: ['~/assets/css/reset.css', '~/assets/css/common.css'],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: `@use "~/assets/scss/variables.scss" as *;`
-        }
-      }
-    }
-  },
-  components: true, // 자동 컴포넌트 로드
-  pages: true, // 파일 기반 라우팅
-  devtools: {
-    enabled: false // 개발자 도구 비활성화
-  },
   app: {
     head: {
+      title: 'Coincident Ventures',
+      meta: [
+        { charset: 'utf-8', hid: 'charset' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+          hid: 'viewport'
+        },
+        {
+          name: 'keywords',
+          content:
+            'Coincident Ventures, 코인자산운용, Blockchain VC, Crypto hedge fund, 가상자산 운용사, 암호화폐, 헤지펀드, 디지털 자산 운용, 블록체인 투자 회사, 크립토VC, 웹3 투자 회사, Web3 투자',
+          hid: 'keywords'
+        },
+        { property: 'og:title', content: 'Coincident Ventures', hid: 'og:title' },
+        {
+          property: 'og:description',
+          content:
+            'Coincident Ventures is the pioneering VC hedge fund dedicated to uncovering and nurturing transformative opportunities across emerging industries.',
+          hid: 'og:description'
+        },
+        { property: 'og:image', content: '/og.jpg', hid: 'og:image' },
+        { property: 'og:url', content: '', hid: 'og:url' },
+        { property: 'og:type', content: 'website', hid: 'og:type' }
+      ],
       link: [
-        // Google Fonts CDN 추가
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined'
@@ -28,5 +38,19 @@ export default defineNuxtConfig({
         }
       ]
     }
-  }
+  },
+  // 나머지 설정 (css, vite 등)
+  css: ['~/assets/css/reset.css', '~/assets/css/common.css'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/scss/variables.scss" as *;`
+        }
+      }
+    }
+  },
+  components: true,
+  pages: true,
+  devtools: { enabled: false }
 })
